058a083209cd10b86b5115453c8926f9d37718af
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-02-21 23:14:15 +0530

    React: Folder Structure Components, Types, Features, Nested Components, Fragments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import Component1 from './components/Component1'
+import Component2 from './components/Component2'
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome class! Now, let's learn REACT!</h1>
+      <Component1 />
+      <Component2 />
     </div>
   );
 }
 
 export default App;
+
+
+/*
+  REACTJS
+
+  Declarative library 
+  - Just tell it WHAT to want 
+  - You don't tell it HOW to do it
+
+  ---------------------------
+  Components - class (Class based components) or function (Function based components) which returns JSX
+
+  - Parts of UI
+  - Can be nested
+  - Can be re-used
+
+  ----------------------------
+
+*/
---
+++ src/components/Component2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Component2(){
+  return (
+    <div className="Component2">
+      <h1>Heading 2</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, cumque voluptatum obcaecati aliquid nam veritatis perspiciatis aperiam eligendi esse harum excepturi ipsum eveniet in a asperiores pariatur itaque ea natus.</p>
+    </div>
+  )
+}
+
+export default Component2
